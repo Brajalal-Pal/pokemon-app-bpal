12c5b70f13ae759a46da97b684372c17181859b6
Brajalal Pal <brajalal@rediffmail.com>
2019-11-03 14:13:38 +0530

    Working copy...

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import DashBoard from "./dashBoard";
+//import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('DashBoard component', function() {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<DashBoard initialUrl="https://w"/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
+  
+
